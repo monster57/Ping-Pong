6ebcd27567e81c3dbcb0036a03bd2802feeab09a
monster57 <surajitbarman57@yahoo.in>
2015-05-24 14:55:31 +0530

    game is running. ball is moving and the platform is also working . the color of the background is also changing

--- PingPong/Form1.Designer.cs
+++ PingPong/Form1.Designer.cs
@@ -29,11 +29,112 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.gameover_lbl = new System.Windows.Forms.Label();
+            this.points_lbl = new System.Windows.Forms.Label();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.gameover_lbl);
+            this.playground.Controls.Add(this.points_lbl);
+            this.playground.Controls.Add(this.score_lbl);
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(918, 499);
+            this.playground.TabIndex = 0;
+            // 
+            // gameover_lbl
+            // 
+            this.gameover_lbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.gameover_lbl.AutoSize = true;
+            this.gameover_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameover_lbl.Location = new System.Drawing.Point(312, 143);
+            this.gameover_lbl.Name = "gameover_lbl";
+            this.gameover_lbl.Size = new System.Drawing.Size(434, 276);
+            this.gameover_lbl.TabIndex = 4;
+            this.gameover_lbl.Text = "Game Over\r\n\r\nspace -Restart \r\nEsc - Quit";
+            this.gameover_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // points_lbl
+            // 
+            this.points_lbl.AutoSize = true;
+            this.points_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.points_lbl.Location = new System.Drawing.Point(221, 9);
+            this.points_lbl.Name = "points_lbl";
+            this.points_lbl.Size = new System.Drawing.Size(63, 69);
+            this.points_lbl.TabIndex = 3;
+            this.points_lbl.Text = "0";
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_lbl.Location = new System.Drawing.Point(12, 9);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(203, 69);
+            this.score_lbl.TabIndex = 2;
+            this.score_lbl.Text = "Score:";
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(75, 81);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.Color.Black;
+            this.racket.Location = new System.Drawing.Point(337, 447);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(200, 20);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 499);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label points_lbl;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label gameover_lbl;
     }
 }
 
--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -12,9 +12,78 @@ namespace PingPong
 {
     public partial class Form1 : Form
     {
+        public int speed_left = 4; // speed of the ball
+        public int speed_top = 4;
+        public int points = 0;       // scored points 
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();          //hide the cursor
+            this.FormBorderStyle = FormBorderStyle.None;        // Remove any border
+            this.TopMost = true;            // Bring the form to the front
+            this.Bounds = Screen.PrimaryScreen.Bounds;  // make it full screen
+
+            racket.Top = playground.Bottom - (playground.Bottom/10); //set the position of racket
+            gameover_lbl.Left = (playground.Width/2) - (gameover_lbl.Width/2); // Position to centre 
+            gameover_lbl.Top = playground.Height/2 - (gameover_lbl.Height/2);
+            gameover_lbl.Visible = false; // Hide
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X - (racket.Width - 2);       // set the centre of the racket to the position of the cursor
+            ball.Left += speed_left;        //Move the ball
+            ball.Top += speed_top;
+
+            if (ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left &&
+                ball.Right <= racket.Right)     //racket collition
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;     //Change direction
+                points += 1;
+                points_lbl.Text = points.ToString();
+                Random r =  new Random();
+                playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+            }
+            if (ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Top <=playground.Top)
+            {
+                speed_top = -speed_top;
+            }
+            if (ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;         // Ball is out -> stop the game
+                gameover_lbl.Visible = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) this.Close();  // Press escape to quit 
+            if (e.KeyCode== Keys.Space)        //Reload game
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                points = 0;
+                points_lbl.Text = "0";
+                timer1.Enabled = true;
+
+                gameover_lbl.Visible = false;
+            }
+
+        }
+
+     
     }
 }
